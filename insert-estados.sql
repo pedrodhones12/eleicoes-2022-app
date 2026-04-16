cd428a4ed43a203ef0713a0a22a2df1e842611fd
pedrodhones12 <99297623+pedrodhones12@users.noreply.github.com>
2026-04-16 13:50:08 -0300

    Add files via upload

---
+++ insert-estados.sql
@@ -0,0 +1,12 @@
+INSERT INTO EstadosEleicao
+(NomeEstado, Eleitorado, Abstencao, PercentualAbstencao,
+ VotosLula, PercentualLula,
+ VotosBolsonaro, PercentualBolsonaro,
+ VotosBrancos, PercentualBrancos,
+ VotosNulos, PercentualNulos)
+VALUES
+('Bahia', 11282999, 2311271, 20.48, 6097815, 72.12, 2357028, 27.88, 121221, 1.35, 395664, 4.41),
+
+('São Paulo', 34684876, 7304385, 21.06, 11519882, 44.76, 14216587, 55.24, 526677, 1.92, 1117345, 4.08),
+
+('Minas Gerais', 16284615, 3418331, 20.99, 6190960, 50.20, 6141310, 49.80, 183206, 1.42, 350808, 2.73);
